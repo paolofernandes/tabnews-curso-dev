bf7024cd84e5e523a8e7cf5e1c5d700e3df1bbf1
Paolo Fernandes <paoloef@gmail.com>
2025-11-18 10:21:42 +0000

    alteração de mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>"15 anos não são 15 dias."<br/>"Eu sou bom no que faço e logo estarei trabalhando novamente."</h1>
+    return <h1>15 anos não são 15 dias.<br/>Eu sou bom no que faço e logo estarei trabalhando novamente.</h1>
 }
 
 export default Home;
