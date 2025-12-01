78f39954301780d0e7e954ad1f3557d3432c7709
Paolo Fernandes <paoloef@gmail.com>
2025-12-01 14:42:11 +0000

    alteração da home

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ function Home() {
       15 anos não são 15 dias.
       <br />
       Eu sou bom no que faço e logo estarei trabalhando novamente.
+      <br />
+      <b>UPDATE:</b> Voltei a trabalhar galera!!!
     </h1>
   );
 }
