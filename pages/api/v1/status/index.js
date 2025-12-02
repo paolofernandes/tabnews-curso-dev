a55312cf2959695366391ffb7b47ece4211d63c0
Paolo Fernandes <paoloef@gmail.com>
2025-12-02 21:10:03 +0000

    add a more robust ssl configuration method

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,27 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query(`
-    SELECT
-        current_setting('server_version') AS db_version,
-        s1.setting::int AS db_max_connections,
-        (SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db') AS db_used_connections
-    FROM
-        pg_settings s1
-    WHERE
-        s1.name = 'max_connections';`);
-  console.log(result.rows[0]);
+  // const result = await database.query(`
+  //   SELECT
+  //       current_setting('server_version') AS db_version,
+  //       s1.setting::int AS db_max_connections,
+  //       (SELECT COUNT(*) FROM pg_stat_activity WHERE datname = 'local_db') AS db_used_connections
+  //   FROM
+  //       pg_settings s1
+  //   WHERE
+  //       s1.name = 'max_connections';`);
+  // console.log(result.rows[0]);
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-  console.log(databaseVersionValue);
 
   const databaseMaxConnectionResult = await database.query(
     "SHOW max_connections;",
   );
   const databaseMaxConnectionValue =
     databaseMaxConnectionResult.rows[0].max_connections;
-  console.log(databaseMaxConnectionValue);
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
@@ -30,7 +28,6 @@ async function status(request, response) {
   });
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
-  console.log(databaseOpenedConnectionsValue);
 
   const updateAt = new Date().toISOString();
   response.status(200).json({
